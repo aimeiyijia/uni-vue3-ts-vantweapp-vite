5af8a2eb8b33d2466b3bcb7b3930671ef8aeebb2
aimeiyijia <1456723200@qq.com>
2023-04-04 18:03:22 +0800

    chore: 增加小程序所用的组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,6 +9,13 @@
     </view>
     <view>1234</view>
     <view @click="changeTitle">changeTitle</view>
+
+    <van-tabbar :active="0">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </view>
 </template>
 
